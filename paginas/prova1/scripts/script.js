554a4a5b944c1d44e0915e5b0d81eea368ff9912
NOVOTEC-NAKA <00001101752452sp@aluno.educacao.sp.gov.br>
2022-08-16 09:45:22 -0300

    mudanças

--- paginas/prova1/scripts/script.js
+++ paginas/prova1/scripts/script.js
@@ -2,3 +2,12 @@ function mudar(imagemSrc, texto) {
     document.getElementById("imagemDisplay").src = imagemSrc;
     document.getElementById("personagem").innerHTML = texto;
 }
+
+function levarWiki() {
+    var personagem = document.getElementById("personagem").innerHTML
+
+    if (personagem == "Dio Brando") {
+        window.open("https://jjba.fandom.com/pt-br/wiki/Dio_Brando")
+    }
+
+}
