6b84b218c29d3e30f0ce75c21778401c14ab101d
NOVOTEC-NAKA <00001101752452sp@aluno.educacao.sp.gov.br>
2022-08-30 11:39:57 -0300

    pronto

--- paginas/form/scripts/script.js
+++ paginas/form/scripts/script.js
@@ -5,6 +5,23 @@ function pegarDado(nome) {
     return urlParams.get(nome)
 }
 
+function set(nome, alt) {
+    document.getElementById(nome+"Display").innerHTML = alt+": " + pegarDado(nome)
+}
+
 function mostrarDados() {
-    document.getElementById("nomeDisplay").innerHTML = "Nome: " + pegarDado("nome")
+    set("nome", "Nome")
+    set("data", "Data de Nascimento")
+    set("éVivo", "Pessoa Vivo")
+   
+    set("temBike", "Tem Bike")
+    set("temCarro", "Tem Carro")
+    set("taiwanExiste", "Taiwan é um País")
+   
+    set("pato", "Pato")
+    set("num", "Número")
+    set("data2", "Escolhe uma Data")
+   
+    set("cor", "Cor Code")
+    set("email", "Email")
 }
